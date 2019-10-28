c3765d9ecc24fd6923c0ab6eae4541d83424af49
m4rk00s <marktony.x3.24@gmail.com>
2019-10-28 17:49:48 +0700

    fix week 4 ex 09

--- week_04/day_03/exercises_09.js
+++ week_04/day_03/exercises_09.js
@@ -1,32 +1,17 @@
-function removeWhitespaces(str) {
-    let res = '';
-
-    for (let char of str) {
-        if (char !== ' ') {
-            res += char;
-        }
-    }
-
-    return res;
-}
-
 function checkAB(num) {
     
-    // remove all whitespaces
-    let temp =  removeWhitespaces(num);
-
     // to store location a or b
     let loc = [];
 
-    for (let ix = 0; ix < temp.length; ix++) {
-        if (temp[ix] === 'a' || temp[ix] === 'b') {
+    for (let ix = 0; ix < num.length; ix++) {
+        if (num[ix] === 'a' || num[ix] === 'b') {
             loc.push(ix);
         }
     }
 
     for (let char1 of loc) {
         for (let char2 of loc) {
-            if (Math.abs(char1 - char2) === 3) {
+            if (Math.abs(char1 - char2) === 4) {
                 return true;
             }
         }
